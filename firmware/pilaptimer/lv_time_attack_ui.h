5d385707ed0f94154793315dece34acd79f71f32
Matigi <34457169+matigi@users.noreply.github.com>
2026-01-27 23:21:43 -0500

    Fix lv_port_disp include order

---
+++ firmware/pilaptimer/lv_time_attack_ui.h
@@ -0,0 +1,25 @@
+#ifndef LV_TIME_ATTACK_UI_H
+#define LV_TIME_ATTACK_UI_H
+
+#ifndef LV_CONF_INCLUDE_SIMPLE
+#define LV_CONF_INCLUDE_SIMPLE
+#endif
+#include <lvgl.h>
+#include "ui_state.h"
+
+struct UiSnapshot {
+  UiState state;
+  uint8_t selectedDriver;
+  uint8_t selectedLaps;
+  uint8_t lapCount;
+  uint32_t sessionMs;
+  uint32_t currentLapMs;
+  uint32_t lastLapMs;
+  uint32_t bestLapMs;
+  int32_t deltaMs;
+};
+
+void lv_time_attack_ui_init(void (*startStopCb)(), void (*resetCb)());
+void lv_time_attack_ui_update(const UiSnapshot &snapshot);
+
+#endif
---
+++ firmware/pilaptimer/ui_state.h
@@ -0,0 +1,13 @@
+#ifndef UI_STATE_H
+#define UI_STATE_H
+
+enum UiState {
+  UI_BOOT,
+  UI_IDLE,
+  UI_ARMED,
+  UI_RUNNING,
+  UI_FINISHED,
+  UI_STATS
+};
+
+#endif
