d059d4ac81ce36cb6611f59392b191d22fd59c09
Matigi <34457169+matigi@users.noreply.github.com>
2026-01-29 17:12:24 -0500

    Add reaction race mode screen and state machine

---
+++ firmware/pilaptimer/screen_reaction.h
@@ -0,0 +1,34 @@
+#pragma once
+
+struct _lv_obj_t;
+typedef struct _lv_obj_t lv_obj_t;
+
+enum ReactionState {
+  REACTION_IDLE,
+  REACTION_ARMED,
+  REACTION_COUNTDOWN,
+  REACTION_READY_RANDOM,
+  REACTION_GO_RUNNING,
+  REACTION_RUN_ACTIVE,
+  REACTION_FALSE_START
+};
+
+struct ReactionUiSnapshot {
+  ReactionState state;
+  uint8_t amberCount;
+  bool greenOn;
+  bool reactionCaptured;
+  uint32_t reactionMs;
+  uint32_t runMs;
+  uint32_t bestReactionMs;
+  uint32_t lastReactionMs;
+};
+
+typedef void (*reaction_handler_t)(void);
+
+void screen_reaction_attach(lv_obj_t *parent);
+void screen_reaction_set_swipe_left_handler(reaction_handler_t cb);
+void screen_reaction_set_swipe_right_handler(reaction_handler_t cb);
+void screen_reaction_set_action_handler(reaction_handler_t cb);
+void screen_reaction_set_arm_handler(reaction_handler_t cb);
+void screen_reaction_update(const ReactionUiSnapshot &snapshot);
