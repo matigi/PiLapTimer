56712be6180f799b679a6ee972c13c8c708531a3
Matigi <34457169+matigi@users.noreply.github.com>
2026-01-29 23:04:34 -0500

    Update images

--- firmware/pilaptimer/boot_splash_v3_280x456_rgb565.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-#include "../../assets/ui/splash/boot_splash_v3_280x456_rgb565.h"
-
-#define BOOT_SPLASH_V3_WIDTH BOOT_SPLASH_WIDTH
-#define BOOT_SPLASH_V3_HEIGHT BOOT_SPLASH_HEIGHT
-#define boot_splash_v3_rgb565 boot_splash_rgb565
